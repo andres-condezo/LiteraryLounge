a819cc1ecbfe27db07f6d2c2bb44b71943c685c9
andres-condezo <ra.condezomonge@gmail.com>
2023-08-11 11:03:58 -0500

    [Fix][Fix book reducer]

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { addBook } from '../redux/booksSlice';
-import library from '../data/books.json';
+import { getBooks } from '../redux/booksSlice';
+import bookObject from '../data/books.json';
 
 function Home() {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(addBook(library));
-  }, []);
-
   const books = useSelector((state) => state.books);
-  console.log('💬 :home.js:Home:14:', books);
+
+  useEffect(() => {
+    dispatch(getBooks(bookObject));
+  }, []);
 
   return (
     <div>
       <ul>
-        {/*
-          <li>{books[0].title}</li>
-        */}
-        <li>{JSON.stringify(books[0])}</li>
+        {books && books.map((book) => <li key={book.title}>{book.title}</li>)}
       </ul>
     </div>
   );
--- src/redux/booksSlice.js
+++ src/redux/booksSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { saveState } from '../logic/localStorage';
 
-const initialState = [];
-
 const BookSlice = createSlice({
   name: 'books',
-  initialState,
+  initialState: [],
   reducers: {
-    addBook: (state, action) => {
-      const library = action.payload;
-      library.library.forEach((book) => {
-        state.push(book.book);
+    getBooks: (state, action) => {
+      const newInitialState = [];
+      const books = action.payload;
+      books.library.forEach((book) => {
+        newInitialState.push({ ...book.book, onReadList: false });
       });
       saveState(state);
+      return newInitialState;
     },
   },
 });
 
-export const { addBook } = BookSlice.actions;
+export const { getBooks } = BookSlice.actions;
 export default BookSlice.reducer;
