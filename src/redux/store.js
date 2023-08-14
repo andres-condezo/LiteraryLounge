1732b8c0d666fa235c1ffb743ef12a0651d36337
Gaby <gaby.es.sd@gmail.com>
2023-08-14 18:12:48 -0500

    Call to fetchAllBooks just after create the app store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './booksSlice';
+import booksReducer, { fetchAllBooks } from './booksSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
   },
 });
+
+store.dispatch(fetchAllBooks());
 
 export default store;
