c01f89b174f6c73b3147fd9449d85a265ba43c5f
Gaby <gaby.es.sd@gmail.com>
2023-08-14 18:09:58 -0500

    Add FilterAside Component into Main component

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,29 @@
-const Main = () => (
-  <main>
-    <span>Main</span>
-  </main>
-);
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import FilterAside from './FilterAside';
+import BooksList from './booksList';
+import getFilteredBooks from '../logic/getFilters';
+
+const Main = () => {
+  const [listaLibros, setListaLibros] = useState([]);
+  // console.log('---------------LISTA LIBROS------------------');
+  // console.log(listaLibros);
+  const bookList = useSelector((state) => state.books);
+
+  const filterHandler = (title = '', genre = '', maxPages = 1000) => {
+    setListaLibros(getFilteredBooks(title, genre, maxPages));
+  };
+
+  useEffect(() => {
+    setListaLibros(bookList);
+  }, [bookList]);
+
+  return (
+    <main>
+      <FilterAside filterHandler={filterHandler} />
+      <BooksList listaLibros={listaLibros} />
+    </main>
+  );
+};
 
 export default Main;
