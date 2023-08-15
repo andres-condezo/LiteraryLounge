08be26f618a8b696d7782b463c9e402994bedfb6
Gaby <gaby.es.sd@gmail.com>
2023-08-15 10:13:54 -0500

    Create FilterAside component

--- src/components/FilterAside.js
+++ src/components/FilterAside.js
@@ -2,19 +2,20 @@ import { PropTypes } from 'prop-types';
 import { useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
 import SelectFilter from './SelectFilter';
+import RangeBar from './RangeBar';
 
 const FilterAside = ({ filterHandler }) => {
   const [titleFilter, setTitleFilter] = useState('');
   const [genreFilter, setGenreFilter] = useState('');
-  // const [pagesFilter, setPagesFilter] = useState('');
+  const [pagesFilter, setPagesFilter] = useState([]);
 
   const setTitleFilterHandler = (title) => setTitleFilter(title);
   const setGenreFilterHandler = (genre) => setGenreFilter(genre);
-  // const setPagesFilterHandler = (pages) => setPagesFilter(pages);
+  const setPagesFilterHandler = (pages) => setPagesFilter(pages);
 
   useEffect(() => {
-    filterHandler(titleFilter, genreFilter);
-  }, [titleFilter, genreFilter]);
+    filterHandler(titleFilter, genreFilter, pagesFilter);
+  }, [titleFilter, genreFilter, pagesFilter]);
 
   return (
     <aside className="aside__filter">
@@ -22,6 +23,7 @@ const FilterAside = ({ filterHandler }) => {
       <SearchBar setTitleFilterHandler={setTitleFilterHandler} />
       <h3>FILTERS</h3>
       <SelectFilter setGenreFilterHandler={setGenreFilterHandler} />
+      <RangeBar setPagesFilterHandler={setPagesFilterHandler} pagesFilter={pagesFilter} />
     </aside>
   );
 };
